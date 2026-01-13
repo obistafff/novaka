29651439f0b0c2e0afc1fc01d53dc2ea2b3fd573
obistafff <currensylla@gmail.com>
2026-01-13 08:16:02 +0100

    feat: shared header and footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,13 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 
 export default function App() {
-  return <Home />;
+  return (
+    <>
+      <Header />
+      <Home />
+      <Footer />
+    </>
+  );
 }
